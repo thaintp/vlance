773373552f0540d2eef30cdd3feb0a674bcbf1d7
snowdence <snowdence2911@gmail.com>
2021-05-14 10:44:58 +0700

    [I-FE]  List applicants and fix  job state

--- src/services/offer.js
+++ src/services/offer.js
@@ -1,15 +1,46 @@
 import axios from "utils/axios";
 
 class OfferService {
-  async make(job_id, balance, expect_day, phone, email, exp_description, step_description) {
+  async make(
+    job_id,
+    balance,
+    expect_day,
+    phone,
+    email,
+    exp_description,
+    step_description
+  ) {
     return await axios({
       method: "POST",
       url: "/offer",
-      data: {job_id, balance, expect_day, phone, email, exp_description, step_description}
-    }).then(res => res.data).catch(err => console.error(err));
+      data: {
+        job_id,
+        balance,
+        expect_day,
+        phone,
+        email,
+        exp_description,
+        step_description,
+      },
+    })
+      .then((res) => res.data)
+      .catch((err) => console.error(err));
+  }
+  async getOffersAPI(params) {
+    var queryString = Object.keys(params)
+      .map((key) => key + "=" + params[key])
+      .join("&");
+
+    return await axios({
+      method: "GET",
+      url: `/offer?${queryString}`,
+    });
   }
   async checkCanOffer(job_id) {
-    return await axios.get(`/check_can_offer?job_id=${job_id}`).then(res => res.data.data).catch(err => console.error(err));
+    return await axios
+      .get(`/check_can_offer?job_id=${job_id}`)
+      .then((res) => res.data.data)
+      .catch((err) => console.error(err));
   }
 }
 
