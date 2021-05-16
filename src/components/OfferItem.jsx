797f417f7d9222d5a6716fc41f06a46b6f955b66
snowdence <snowdence2911@gmail.com>
2021-05-17 03:33:25 +0700

    Revert "Merge remote-tracking branch 'HBTghost/thai_develop' into develop"
    This reverts commit bbae2e18ac3160aa5c44fbfeba799d2129cab807.

--- src/components/OfferItem.jsx
+++ src/components/OfferItem.jsx
@@ -20,7 +20,6 @@ const OfferItem = ({ offer, job }) => {
           icon: "success",
           title: "Chọn ứng viên thành công",
         });
-        window.location.reload();
       } else {
         Toast.fire({
           icon: "error",
