1e1e6a3da42016748132f8557a0aa6b6ed736ce5
snowdence <snowdence2911@gmail.com>
2021-05-17 03:33:25 +0700

    Revert "Merge remote-tracking branch 'HBTghost/thai_develop' into develop"
    This reverts commit bbae2e18ac3160aa5c44fbfeba799d2129cab807.

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,46 +1,34 @@
 import "./style.scss";
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
-import CategoryService from "services/category";
-import classnames from "classnames";
-
-const Category = ({ onChange }) => {
-  const [category, setCategory] = useState({});
-  const [categories, setCategories] = useState([]);
-  useEffect(() => {
-    CategoryService.get().then((data) => {
-      setCategories(data.data);
-    });
-  }, []);
-
-  
-  const handleOnclick = (item) => {
-    console.log(item)
-    setCategory(item);
-    onChange(item);
-  };
-
+const Category = () => {
   return (
     <div className="category">
       <div className="sidebar__title sidebar__text--bold">Category</div>
       <div className="category__list">
-        <Link to="#" className={classnames("category__tag bold-link", {
-              "bold-link--active": category.id === null,
-            })} onClick={() => handleOnclick({id: null})}>
+        <Link to="#" className="category__tag--active">
           All dresses
         </Link>
-        {categories.map((c) => (
-          <Link
-            to="#"
-            className={classnames("category__tag bold-link", {
-              "bold-link--active": category.id === c.id,
-            })}
-            key={c.id}
-            onClick={() => handleOnclick(c)}
-          >
-            {c.name}
-          </Link>
-        ))}
+        <Link to="#" className="category__tag">
+          Rompers / Jumpsuits
+        </Link>
+        <Link to="#" className="category__tag">
+          Casual dresses
+        </Link>
+        <Link to="#" className="category__tag">
+          Going out dresses
+        </Link>
+        <Link to="#" className="category__tag">
+          Party / Occasion dresses
+        </Link>
+        <Link to="#" className="category__tag">
+          Mini dresses
+        </Link>
+        <Link to="#" className="category__tag">
+          Maxi / Midi dresses
+        </Link>
+        <Link to="#" className="category__tag">
+          Sets
+        </Link>
       </div>
     </div>
   );
